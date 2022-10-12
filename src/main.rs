1a8e48928b1f4dd3b070cbef702b9269bce52405
Victor Wang <victorwang2001@gmail.com>
2022-10-12 04:06:18 +0000

    create structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+struct Game {
+    squares: [[Square; 8]; 8],
+    player_turn: Color,
 }
+
+trait ChessGame {
+    // fn pretty_print(&self);
+    // fn to_fen_str(&self);
+}
+
+impl ChessGame for Game {}
+
+trait Playable {
+    // fn move_is_valid(&self, Move);
+    // fn make_move(&self, Move);
+}
+
+impl Playable for Game {}
+
+struct Pos {
+    row: i16,
+    col: i16,
+}
+
+struct Square {
+    pos: Pos,
+    piece: Option<Piece>,
+}
+
+struct Piece {
+    piece_type: PieceType,
+    color: Color,
+    has_moved: bool, // if the piece has been moved at least once before, useful for castle and pawn start
+}
+
+enum Color {
+    Black = 0,
+    White,
+}
+
+enum PieceType {
+    Pawn = 0,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+}
+
+struct Move {
+    startPos: Pos,
+    endPos: Pos,
+}
+
+fn main() {}
