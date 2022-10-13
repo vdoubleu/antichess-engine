6d9144d072b5ddae809414efb9ad108a3fcb6dc1
vdoubleu <victorwang2001@gmail.com>
2022-10-12 21:47:32 -0400

    fen support

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,21 @@ struct Game {
 impl fmt::Display for Game {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut out_string = String::from(format!("Turn: {}\n", self.player_turn));
-      
+
         let divider = "-----------------\n";
 
         out_string += divider;
 
         for r in 0..8 {
-          out_string += "|";
-          for c in 0..8 {
-            out_string += self.squares[r][c].char_notation().as_str();
             out_string += "|";
-          }
-          out_string += "\n";
-          out_string += divider;
+            for c in 0..8 {
+                out_string += self.squares[r][c].char_notation().as_str();
+                out_string += "|";
+            }
+            out_string += "\n";
+            out_string += divider;
         }
-      
+
         write!(f, "{}", out_string)
     }
 }
@@ -47,7 +47,7 @@ impl Game {
     fn add_piece(&mut self, piece: PieceType, color: Color, pos: Pos) -> &mut Self {
         self.squares[pos.row][pos.col].piece = Some(Piece::new(piece, color));
 
-      self
+        self
     }
 
     fn empty_board(&mut self) -> &mut Self {
@@ -62,27 +62,74 @@ impl Game {
 
     fn set_starting_pos(&mut self) -> &mut Self {
         for c in 0..8 {
-          self.add_piece(PieceType::Pawn, Color::Black, Pos::new(1, c))
-            .add_piece(PieceType::Pawn, Color::White, Pos::new(6, c));
+            self.add_piece(PieceType::Pawn, Color::Black, Pos::new(1, c))
+                .add_piece(PieceType::Pawn, Color::White, Pos::new(6, c));
         }
 
         self.add_piece(PieceType::Rook, Color::Black, Pos::new(0, 0))
-          .add_piece(PieceType::Rook, Color::White, Pos::new(7, 0))
-          .add_piece(PieceType::Knight, Color::Black, Pos::new(0, 1))
-          .add_piece(PieceType::Knight, Color::White, Pos::new(7, 1))
-          .add_piece(PieceType::Bishop, Color::Black, Pos::new(0, 2))
-          .add_piece(PieceType::Bishop, Color::White, Pos::new(7, 2))
-          .add_piece(PieceType::Queen, Color::Black, Pos::new(0, 3))
-          .add_piece(PieceType::Queen, Color::White, Pos::new(7, 3))
-          .add_piece(PieceType::King, Color::Black, Pos::new(0, 4))
-          .add_piece(PieceType::King, Color::White, Pos::new(7, 4))
-          .add_piece(PieceType::Bishop, Color::Black, Pos::new(0, 5))
-          .add_piece(PieceType::Bishop, Color::White, Pos::new(7, 5))
-          .add_piece(PieceType::Knight, Color::Black, Pos::new(0, 6))
-          .add_piece(PieceType::Knight, Color::White, Pos::new(7, 6))
-          .add_piece(PieceType::Rook, Color::Black, Pos::new(0, 7))
-          .add_piece(PieceType::Rook, Color::White, Pos::new(7, 7));
-        
+            .add_piece(PieceType::Rook, Color::White, Pos::new(7, 0))
+            .add_piece(PieceType::Knight, Color::Black, Pos::new(0, 1))
+            .add_piece(PieceType::Knight, Color::White, Pos::new(7, 1))
+            .add_piece(PieceType::Bishop, Color::Black, Pos::new(0, 2))
+            .add_piece(PieceType::Bishop, Color::White, Pos::new(7, 2))
+            .add_piece(PieceType::Queen, Color::Black, Pos::new(0, 3))
+            .add_piece(PieceType::Queen, Color::White, Pos::new(7, 3))
+            .add_piece(PieceType::King, Color::Black, Pos::new(0, 4))
+            .add_piece(PieceType::King, Color::White, Pos::new(7, 4))
+            .add_piece(PieceType::Bishop, Color::Black, Pos::new(0, 5))
+            .add_piece(PieceType::Bishop, Color::White, Pos::new(7, 5))
+            .add_piece(PieceType::Knight, Color::Black, Pos::new(0, 6))
+            .add_piece(PieceType::Knight, Color::White, Pos::new(7, 6))
+            .add_piece(PieceType::Rook, Color::Black, Pos::new(0, 7))
+            .add_piece(PieceType::Rook, Color::White, Pos::new(7, 7));
+
+        self
+    }
+
+    fn to_fen_notation(&self) -> String {
+        let mut fen_string = String::new();
+
+        for r in 0..8 {
+            let mut empty_count = 0;
+            for c in 0..8 {
+                if let Some(piece) = &self.squares[r][c].piece {
+                    if empty_count > 0 {
+                        fen_string += empty_count.to_string().as_str();
+                        empty_count = 0;
+                    }
+                    fen_string += piece.char_notation().as_str();
+                } else {
+                    empty_count += 1;
+                }
+            }
+            if empty_count > 0 {
+                fen_string += empty_count.to_string().as_str();
+            }
+            if r < 7 {
+                fen_string += "/";
+            }
+        }
+
+        fen_string
+    }
+
+    fn from_fen_notation(&mut self, fen_str: String) -> &mut Self {
+        let mut row: usize = 0;
+        let mut col: usize = 0;
+
+        for c in fen_str.chars() {
+            if c == '/' {
+                row += 1;
+                col = 0;
+            } else if c.is_digit(10) {
+                col += c.to_digit(10).unwrap() as usize;
+            } else {
+                let piece = Piece::from_char(c);
+                self.squares[row][col].piece = Some(piece);
+                col += 1;
+            }
+        }
+
         self
     }
 }
@@ -104,7 +151,7 @@ impl Pos {
     fn default() -> Pos {
         Pos { row: 0, col: 0 }
     }
-  
+
     fn new(r: usize, c: usize) -> Pos {
         Pos { row: r, col: c }
     }
@@ -123,12 +170,12 @@ impl Square {
             piece: None,
         }
     }
-  
+
     fn char_notation(&self) -> String {
-      match self.piece {
-        Some(p) => p.char_notation(),
-        None => String::from(" "),
-      }
+        match self.piece {
+            Some(p) => p.char_notation(),
+            None => String::from(" "),
+        }
     }
 }
 
@@ -144,20 +191,39 @@ struct Piece {
 }
 
 impl Piece {
-  fn new(piece_type: PieceType, color: Color) -> Piece {
-    Piece {
-      piece_type: piece_type,
-      color: color,
-      has_moved: false,
+    fn new(piece_type: PieceType, color: Color) -> Piece {
+        Piece {
+            piece_type: piece_type,
+            color: color,
+            has_moved: false,
+        }
     }
-  }
-  
-  fn char_notation(&self) -> String {
-    match self.color {
-      Color::Black => self.piece_type.char_notation().to_lowercase(),
-      Color::White => self.piece_type.char_notation(),
+
+    fn from_char(c: char) -> Piece {
+        let piece_type = match c {
+            'P' | 'p' => PieceType::Pawn,
+            'R' | 'r' => PieceType::Rook,
+            'N' | 'n' => PieceType::Knight,
+            'B' | 'b' => PieceType::Bishop,
+            'Q' | 'q' => PieceType::Queen,
+            'K' | 'k' => PieceType::King,
+            _ => panic!("Invalid piece type"),
+        };
+
+        let color = match c.is_uppercase() {
+            true => Color::White,
+            false => Color::Black,
+        };
+
+        Piece::new(piece_type, color)
     }
-  }
+
+    fn char_notation(&self) -> String {
+        match self.color {
+            Color::Black => self.piece_type.char_notation().to_lowercase(),
+            Color::White => self.piece_type.char_notation(),
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -183,17 +249,17 @@ enum PieceType {
 }
 
 impl PieceType {
-  fn char_notation(&self) -> String {
-    let c = match self {
-        PieceType::Pawn => "P",
-        PieceType::Knight => "N",
-        PieceType::Bishop => "B",
-        PieceType::Rook => "R",
-        PieceType::Queen => "Q",
-        PieceType::King => "K",
-    };
-    String::from(c)
-  }
+    fn char_notation(&self) -> String {
+        let c = match self {
+            PieceType::Pawn => "P",
+            PieceType::Knight => "N",
+            PieceType::Bishop => "B",
+            PieceType::Rook => "R",
+            PieceType::Queen => "Q",
+            PieceType::King => "K",
+        };
+        String::from(c)
+    }
 }
 
 struct Move {
@@ -205,7 +271,8 @@ fn main() {
     let mut game = Game::new();
     println!("{}", game);
 
-    game.set_starting_pos();
+    //game.set_starting_pos();
+    game.from_fen_notation("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR".to_string());
 
     println!("{}", game);
 }
