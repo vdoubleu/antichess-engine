54322ff6e95b724d6a7b28396beceba1c81d1285
vdoubleu <victorwang2001@gmail.com>
2022-10-31 21:00:15 -0400

    alpha beta done

--- src/engine/alpha_beta.rs
+++ src/engine/alpha_beta.rs
@@ -3,7 +3,7 @@ use crate::engine::evaluate_game::evaluate;
 
 /// Implements the min max algorithm (without alpha beta pruning for now) to decide the best move
 /// to play. White is maximizing, black is minimizing.
-pub fn min_max(game: &Game, color: Color) -> Option<ChessMove> {
+pub fn alpha_beta(game: &Game, color: Color) -> Option<ChessMove> {
     let max_depth = 3;
     let mut best_move = None;
 
@@ -23,7 +23,13 @@ pub fn min_max(game: &Game, color: Color) -> Option<ChessMove> {
         let mut game_copy = game.clone();
         game_copy.make_move(&chess_move);
 
-        let score = min_max_impl(&game_copy, color.opposite(), max_depth);
+        let score = alpha_beta_impl(
+            &game_copy,
+            color.opposite(),
+            f64::NEG_INFINITY,
+            f64::INFINITY,
+            max_depth,
+        );
 
         if score > best_score {
             best_score = score;
@@ -34,14 +40,14 @@ pub fn min_max(game: &Game, color: Color) -> Option<ChessMove> {
     best_move
 }
 
-fn min_max_impl(game: &Game, color: Color, depth: u32) -> f64 {
+fn alpha_beta_impl(game: &Game, color: Color, alpha: f64, beta: f64, depth: u32) -> f64 {
     if depth == 0 {
         return evaluate(game);
     }
 
-    let mut best_score: f64 = f64::NEG_INFINITY;
+    let mut best_eval: f64 = f64::NEG_INFINITY;
     if color == Color::Black {
-        best_score = f64::INFINITY;
+        best_eval = f64::INFINITY;
     }
 
     let mut all_valid_moves = game.all_valid_moves_for_color_that_take(color);
@@ -50,37 +56,56 @@ fn min_max_impl(game: &Game, color: Color, depth: u32) -> f64 {
         all_valid_moves = game.all_valid_moves_for_color(color);
     }
 
+    let mut curr_alpha = alpha;
+    let mut curr_beta = beta;
+
     for move_option in all_valid_moves {
         let mut new_game = game.clone();
         new_game.make_move(&move_option);
 
-        let res = min_max_impl(&new_game, color.opposite(), depth - 1);
+        let eval = alpha_beta_impl(
+            &new_game,
+            color.opposite(),
+            curr_alpha,
+            curr_beta,
+            depth - 1,
+        );
 
         if color == Color::White {
-            best_score = res.max(best_score);
+            best_eval = eval.max(best_eval);
+            curr_alpha = curr_alpha.max(eval);
+
+            if beta <= alpha {
+                break;
+            }
         } else {
-            best_score = res.min(best_score);
+            best_eval = eval.min(best_eval);
+            curr_beta = curr_beta.min(eval);
+
+            if beta <= alpha {
+                break;
+            }
         }
     }
 
-    best_score
+    best_eval
 }
 
 #[cfg(test)]
-mod min_max_tests {
+mod alpha_beta_tests {
     use super::*;
     use crate::chess_game::{Color, Game};
 
     #[test]
-    fn test_min_max() {
+    fn test_alpha_beta() {
         let mut game = Game::new_starting_game();
 
-        let move1 = min_max(&game, Color::White);
+        let move1 = alpha_beta(&game, Color::White);
         assert!(move1.is_some());
 
         game.make_move(&move1.unwrap());
 
-        let move2 = min_max(&game, Color::Black);
+        let move2 = alpha_beta(&game, Color::Black);
         assert!(move2.is_some());
 
         game.make_move(&move2.unwrap());
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,13 +1,13 @@
+mod alpha_beta;
 mod evaluate_game;
-mod min_max;
 mod random;
 
 use crate::chess_game::{ChessMove, Color, Game};
-use crate::engine::min_max::min_max;
+use crate::engine::alpha_beta::alpha_beta;
 
 pub fn generate_move(game: &Game, color: Color) -> Option<ChessMove> {
     // right now we'll implement a basic strategy of just picking a random move
     // if we can take, we must first take
     // random_move(game, color)
-    min_max(game, color)
+    alpha_beta(game, color)
 }
