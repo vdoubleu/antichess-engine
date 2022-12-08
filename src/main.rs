bd6472790d37a23018051a8ceb1bbddbdf126415
vdoubleu <victorwang2001@gmail.com>
2022-12-08 15:10:39 -0500

    downgrade clap to 3

--- src/main.rs
+++ src/main.rs
@@ -7,36 +7,8 @@ use anyhow::Result;
 
 use pleco::{BitMove, Board, Player};
 
-use clap::Parser;
+use clap::{App, Arg};
 use std::io::{self, BufRead};
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq, clap::ValueEnum)]
-pub enum ColorArg {
-    Black = 0,
-    White = 1,
-}
-
-impl ColorArg {
-    pub fn to_player(self) -> Player {
-        match self {
-            ColorArg::Black => Player::Black,
-            ColorArg::White => Player::White,
-        }
-    }
-}
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// the color of the engine_color
-    #[clap(value_enum, default_value = "white")]
-    color: ColorArg,
-
-    /// Debug level, -1, 0, 1, or 2. -1 is zero debug output, this is for actually playing games. 0
-    /// is for no search progress output, 1 is for basic search progress output, 2 is for verbose
-    #[clap(short, long, default_value = "-1")]
-    debug: i8,
-}
 
 fn print_move_list(moves: &Vec<BitMove>) {
     eprintln!("valid moves: ");
@@ -72,17 +44,47 @@ fn print_winner(player: Player, is_draw: bool) {
 }
 
 fn main() {
-    let args = Args::parse();
+    // we are now using clap 3, so we have to downgrade from clap 4
+    let matches = App::new("Anti-fish")
+        .version("0.1.0")
+        .author("Victor Wang, Patrick Wang, Jae Yoon Cha")
+        .about("The Anti-fish engine")
+        .arg(
+            Arg::with_name("color")
+                .value_name("COLOR")
+                .help("the color of the engine")
+                .takes_value(true)
+                .possible_values(&["black", "white"])
+                .default_value("white"),
+        )
+        .arg(
+            Arg::with_name("debug")
+                .long("debug")
+                .value_name("DEBUG")
+                .help("Debug level, -1, 0, 1, or 2. -1 is zero debug output, this is for actually playing games. 0 is for no search progress output, 1 is for basic search progress output, 2 is for verbose")
+                .takes_value(true)
+                .default_value("-1"),
+        )
+        .get_matches();
+
+    let your_color = match matches.value_of("color") {
+        Some("black") => Player::Black,
+        Some("white") => Player::White,
+        _ => panic!("Invalid color"),
+    };
+
+    let debug_level = match matches.value_of("debug").unwrap().parse::<i8>() {
+        Ok(n) => n,
+        Err(_) => panic!("Invalid debug level"),
+    };
 
     let mut board = Board::start_pos();
 
     let stdin = io::stdin();
 
-    let your_color = args.color.to_player();
-
     let mut engine = Engine::new();
     engine.opening_book = Some(OpeningBook::new());
-    engine.params.debug_print = args.debug;
+    engine.params.debug_print = debug_level;
 
     if your_color == Player::White {
         let m = match generate_with_fallback(&mut engine, &board) {
@@ -91,7 +93,7 @@ fn main() {
                 m
             }
             Err(e) => {
-                if args.debug > -1 {
+                if debug_level > -1 {
                     eprintln!("encountered error while generating move: {}", e);
                     println!("No moves available");
                 }
@@ -102,7 +104,7 @@ fn main() {
         board.apply_move(m);
     }
 
-    if args.debug > -1 {
+    if debug_level > -1 {
         eprintln!("{}", board);
 
         let opp_valid_moves = engine.generate_valid_moves(&board);
@@ -120,7 +122,7 @@ fn main() {
                 }
 
                 if board.checkmate() || board.stalemate() {
-                    if args.debug > -1 {
+                    if debug_level > -1 {
                         println!("Game over. winner: {}", board.turn().other_player());
                         eprintln!("{}", board);
                         eprintln!("ply: {}", board.ply());
@@ -138,7 +140,7 @@ fn main() {
                         m
                     }
                     Err(e) => {
-                        if args.debug > -1 {
+                        if debug_level > -1 {
                             eprintln!("encountered error in move gen: {}", e);
                             eprintln!("resign");
                         }
@@ -149,7 +151,7 @@ fn main() {
                 board.apply_move(m);
 
                 if board.checkmate() || board.stalemate() {
-                    if args.debug > -1 {
+                    if debug_level > -1 {
                         println!("Game over. winner: {}", board.turn().other_player());
                         eprintln!("{}", board);
                         eprintln!("ply: {}", board.ply());
@@ -161,7 +163,7 @@ fn main() {
                     return;
                 }
 
-                if args.debug > -1 {
+                if debug_level > -1 {
                     eprintln!("{}", board);
 
                     let opp_valid_moves = engine.generate_valid_moves(&board);
@@ -169,7 +171,7 @@ fn main() {
                 }
             }
             Err(error) => {
-                if args.debug > -1 {
+                if debug_level > -1 {
                     println!("error: {}", error)
                 }
             }
